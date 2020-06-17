8104c60fabd5c06e71ac6172a8f35db218b28f01
Nabil Fitrah Abdullah <nabilfitrah0@gmail.com>
2020-06-17 23:55:27 +0700

    added new source in scrapping

--- node/scrapping.js
+++ node/scrapping.js
@@ -33,5 +33,44 @@ function tribunScrapFunc(url){
         }
     });
 }
+
+function merdekaScrapFunc(url){
+    request.get(url,(err, res, body)=>{
+        if (!err && res.statusCode == 200) {
+            const $ = cheerio.load(body);
+            const titles = [];
+            const mdk = $('a[class="mdk-tag-contln-title"]');
+            mdk.each((i,el)=>{
+                const title = $(el).text();
+                let link = $(el).attr('href');
+                link = 'https://www.merdeka.com'+link;
+                console.log(title,'\n',link,'\n');
+            });
+        }
+    });
+}
+
+function detikScrapFunc(url){
+    request(url,(err, res, body)=>{
+        if (!err && res.statusCode == 200) {
+            const $ = cheerio.load(body);
+            const titles = [];
+            const articles = $('article[class="list-content__item"]');
+            articles.each((i,el)=>{
+                const title = $(el).find('div > div.media__text > h3 > a').text().trim();
+                const link = $(el).find('div > div.media__text > h3 > a').attr('href');
+            })
+        }
+    });
+}
+
+
+
+
+
+
+
 kompasScrapFunc('https://www.kompas.com/');
 tribunScrapFunc('https://www.tribunnews.com/')
+detikScrapFunc('https://news.detik.com/indeks?tag_from=wp_nhl_indeks&_ga=2.135661560.892063700.1592404166-1540127914.1567149922');
+merdekaScrapFunc('https://www.merdeka.com/berita-hari-ini/');
